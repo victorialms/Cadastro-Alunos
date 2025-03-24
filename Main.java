ad94d5a00577d1df0c18faef82e0ac1395d598b7
Victoria Lacerda <victoria.simeao@soulasalle.com.br>
2025-03-24 17:56:19 -0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,8 @@
+import controle.ControlaAluno;
+
+public class Main {
+    public static void main(String[] args) {
+        ControlaAluno controle = new ControlaAluno();
+        controle.exibirMenu();
+    }
+}
